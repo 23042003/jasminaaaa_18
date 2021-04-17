2fdfbffb31b6d04a9b9a1917f5d1fab3d7bf43c6
23042003 <81444950+23042003@users.noreply.github.com>
2021-04-17 17:49:04 +0200

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript: Hallo Welt</title>
+    <script>
+      alert("Hallo Welt!");
+    </script>
+    <noscript>
+      Sie haben JavaScript deaktiviert.
+    </noscript>
+  </head>
+  <body>
+    <p>hallo</p>
+  </body>
+</html>
---
+++ js.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+data: [{
+	category: 'Geography',
+	question: 'What‘s the Capital of Germany?',
+	solution: 'Berlin',
+	falses: ['München', 'Bonn', 'Frankfurt'],
+	explanation: 'Erklärung'
+	}, {
+	category: 'HTML',
+	question: 'Welches Markup führt dazu, dass das Element per Tastatur&shy;steuerung anwählbar ist?',
+	solution: '&lt;span tabindex="0">Pick me!&lt;/span>',
+	falses: ['&lt;a class="button">Pick me!&lt;/a>', '&lt;span role="button">Pick me!&lt;/span>'],
+	explanation: 'Durch tabindex können Sie jedes Element fokussierbar machen, ein Wert von 0 behält dabei die generische Reihenfolge bei.'
+	}],
